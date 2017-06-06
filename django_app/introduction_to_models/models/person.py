5ee9b1de40321cedb238062d4531b823e92a0731
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-06-06 16:45:27 +0900

    introduction_to_models app의 models패키지화

--- django_app/introduction_to_models/models/person.py
+++ django_app/introduction_to_models/models/person.py
@@ -39,21 +39,3 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
-
-
-class Car(models.Model):
-    name = models.CharField(max_length=40)
-    manufacturer = models.ForeignKey(
-        Manufacturer,
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.name
